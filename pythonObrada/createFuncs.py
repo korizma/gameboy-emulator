c57732c642b075f116dd00b047fa3cd19b9273cc
korizma desktop <dulekorizma55@gmail.com>
2025-04-06 23:38:13 +0200

    finished all the mnemonics, now to figure out flags:__)

--- pythonObrada/createFuncs.py
+++ pythonObrada/createFuncs.py
@@ -17,14 +17,12 @@ def create_function(instrc, info):
         function_body = get_inc_body(info).replace("inc", "dec")
 
     elif info["mnemonic"] == "RLCA":
-        function_body = "rotLRegA();"
         function_body = "regs[0] <<= 1;"
 
     elif info["mnemonic"] == "ADD":
         function_body = get_add_body(info)
 
     elif info["mnemonic"] == "RRCA":
-        function_body = "rotRRegA();"
         function_body = "regs[0] >>= 1;"
 
     elif info["mnemonic"] == "STOP":
@@ -55,7 +53,6 @@ def create_function(instrc, info):
     elif info["mnemonic"] == "HALT":
         function_body = "// time.sleep(10);"
 
-    
     elif info["mnemonic"] == "ADC":
         operand1 = info['operands'][0]
         function_body = get_sub_body(info).replace('-', '+') + "\n\t" + "reg[0] += (u8)flag(C);"
@@ -77,7 +74,7 @@ def create_function(instrc, info):
         function_body = get_sub_body(info).replace('-', '|')
     
     elif info["mnemonic"] == "CP":
-        # needs seperate function, is just for flags
+        # only affects flags
         pass
 
     elif info["mnemonic"] == "RET":
@@ -86,8 +83,66 @@ def create_function(instrc, info):
     elif info["mnemonic"] == "POP":
         function_body = get_pop_body(info)
 
-    return name + "\n{\n" + "\t{function_body}\n" + "}\n"
+    elif info["mnemonic"] == "JP":
+        function_body = get_jp_body(info)
+    
+    elif info["mnemonic"] == "CALL":
+        function_body = get_call_body(info)
+    
+    elif info["mnemonic"] == "PUSH":
+        function_body = get_push_body(info)
 
+    elif info["mnemonic"] == "RST":
+        function_body = get_rst_body(info)
+
+    elif info["mnemonic"] == "EI":
+        function_body = "setIME();"
+
+    elif info["mnemonic"] == "RETI":
+        function_body = "setIME();\n" + "writeReg(PC, readMem(readReg(SP)));\n" + "\tincSP();\n" + "writeReg(PC, readMem(readReg(SP)));\n" + "\tincSP();\n"
+
+    elif info["mnemonic"] == "LDH":
+        function_body = get_ldh_body(info)
+    
+    elif info["mnemonic"] == "DI":
+        function_body = "resetIME();"
+
+    elif info["mnemonic"] == "RLC":
+        function_body = get_rlc_body(info)
+    
+    elif info["mnemonic"] == "RRC":
+        function_body = get_rlc_body(info).replace("<<", ">>")
+
+    elif info["mnemonic"] == "RL":
+        function_body = get_rl_body(info)
+
+    elif info["mnemonic"] == "RR":
+        function_body = get_rr_body(info)
+
+    elif info["mnemonic"] == "SLA":
+        function_body = get_sla_body(info)
+
+    elif info["mnemonic"] == "SRA":
+        function_body = get_sra_body(info)
+
+    elif info["mnemonic"] == "SWAP":
+        function_body = get_swap_body(info)
+    
+    elif info["mnemonic"] == "SRL":
+        function_body = get_srl_body(info)
+
+    elif info["mnemonic"] == "BIT":
+        pass
+        # sets flag
+
+    elif info["mnemonic"] == "RES":
+        function_body = get_res_body(info)
+    
+    elif info["mnemonic"] == "SET":
+        function_body = get_set_body(info)
+
+    return name + "\n{\n" + "\t{function_body}\n" + "}\n"
+# add to this
 def get_ld_body(info):
     operand1, operand2 = info["operands"][0], info["operand"][1]
 
@@ -101,12 +156,17 @@ def get_ld_body(info):
     elif operand2 == "[a16]":
         read_part = "readMem(byte2 + ((u16)byte3 << 8))"
 
-    elif operand2 == "SP":
-        pass
+    elif operand2 == "SP" and operand1 == "a16":
+        return "writeMem(byte2 + ((u16)byte3 << 8), (u8)readReg(SP));\n" + "writeMem(byte2 + ((u16)byte3 << 8) + 1, readReg(SP) >> 8);"
         # special case, see 0xF8, 0x08
+    elif operand2 == "SP":
+        return "int spreg = readReg(SP);\n" + "writeReg(SP, readReg(SP) + byte2);\n" + "writeReg(HL, readReg(SP));\n" #+ "return 3 * (spreg >> 7 & byte2 >> 7) + 12* ((spreg >> 3) & 1 & (byte >> 3));" 
     
-    elif operand2[0] == "[":
+    elif operand2[0] == "[" and not len(operand2) == 4:
         read_part = "readMem(readReg({operand2[1:-1]}))"
+
+    elif operand2 == "[HL+]" or operand2 == "[HL-]":
+        read_part = "readMem(readReg(HL))"
 
     else: # register load case
         read_part = "readReg({operand2})"
@@ -116,20 +176,22 @@ def get_ld_body(info):
     if operand1 == "[a16]": # this is not 100% correct it could fail
         write_part = "writeMem(byte2 + ((u16)byte3 << 8), {read_part});"
 
-    elif operand1 == "SP":
-        pass
-        #special case see 0xF9, 0x31
-
     elif operand1[0] == "[":
         write_part = "writeMem(readReg({operand1[1:-1]}), {read_part});"
 
     else: # register write case
         write_part = "writeReg({operand1}, {read_part});"
 
-    return write_part
+    if operand1 == "[HL+]" or operand2 == "[HL+]":
+        write_part += "\nwriteReg(HL, readReg(HL) + 1)"
+
+    if operand1 == "[HL-]" or operand2 == "[HL-]":
+        write_part += "\nwriteReg(HL, readReg(HL) - 1)"
+
+    return write_part # + "\n" + "return 0 - 1;"
 
 def get_nop_body(info):
-    return "// bukvalno nista ne radi"
+    return "// nothing"
 
 def get_inc_body(info):
     operand = info["operands"][0]
@@ -141,6 +203,7 @@ def get_inc_body(info):
 
     else:
         body = "incReg({operand});"
+
 
     return body
 
@@ -222,5 +285,152 @@ def get_pop_body(info):
 
     return "\twriteReg({reg[1]}, readMem(readReg(SP)));\n" + "\tincSP();\n" + "\twriteReg({reg[0]}, readMem(readReg(SP)));\n" + "\tincSP();\n"
 
+def get_jp_body(info):
+    operand = info["operands"][0]
 
+    if operand == 'HL':
+        return "writeReg(PC, readReg(HL));"
+    
+    elif operand == 'n16':
+        return "writeReg(PC, byte2 + ((u16)byte3 << 8));"
+    
+    elif operand == "C":
+        return "if (flag(C)) writeReg(PC, byte2 + ((u16)byte3 << 8));"
+    elif operand == "NC":
+        return "if (!flag(C)) writeReg(PC, byte2 + ((u16)byte3 << 8));"
+    
+    elif operand == "Z":
+        return "if (flag(Z)) writeReg(PC, byte2 + ((u16)byte3 << 8));"
+    elif operand == "C":
+        return "if (!flag(Z)) writeReg(PC, byte2 + ((u16)byte3 << 8));"
+    else:
+        return "// mistake"
 
+def get_call_body(info):    # check again
+    operand = info['operands'][0]
+
+    body1 = 'decSP();\n'
+    body2 = 'writeMem(readReg(SP), readReg(PC) + 1);\n'
+    body3 = 'writeReg(PC, byte2 + ((u16)byte3 << 8));\n'
+
+    if operand == 'Z':
+        return "if (flag(Z)) " + "{\n" + body1 + body2 + body3 + "}"
+    elif operand == 'NZ':
+        return "if (!flag(Z)) " + "{\n" + body1 + body2 + body3 + "}"
+    elif operand == 'C':
+        return "if (flag(C)) " + "{\n" + body1 + body2 + body3 + "}"
+    elif operand == 'NC':
+        return "if (!flag(C)) " + "{\n" + body1 + body2 + body3 + "}"
+    else:
+        return body1 + body2 + body3
+    
+def get_push_body(info):    # check again
+    operand = info['operands'][0]
+    reg1 = operand[0]
+    reg2 = operand[1]
+
+    inst1 = "decSP();\n"
+    inst2 = "writeMem(readReg(SP), readReg({reg1}));\n"
+    
+    if reg2 == "F":
+        return inst1 + inst2 + inst1 + "writeMem(readReg(SP), readReg(F) << 4);\n"
+    else:
+        return inst1 + inst2 + inst1 + "writeMem(readReg(SP), readReg({reg2}));\n"
+    
+def get_rst_body(info):
+    operand = info['operands'][0]
+    address = int(operand, 16)
+
+    body1 = 'decSP();\n'
+    body2 = 'writeMem(readReg(SP), readReg(PC) + 1);\n'
+    body3 = 'writeReg(PC, {address});'
+
+    return body1 + body2 + body3
+
+def get_ldh_body(info):
+    operand1, operand2 = info["operands"][0], info["operand"][1]
+
+    if operand2 == "A":
+        if operand1 == "a8":
+            return "writeMem(0xFF00 + byte2, readReg(A));"
+        else:
+            return "writeMem(0xFF00 + readReg(C), readReg(A));"
+        
+    elif operand2 == "a8":
+            return "writeReg(A, 0xFF00 + byte2);"
+    elif operand2 == "[C]":
+            return "writeReg(A, 0xFF00 + readReg(C));"
+
+    return "// this should not exist"
+
+def get_rlc_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), readMem(readReg(HL)) << 1);"
+    else:
+        return "writeReg({operand1}, readReg({operand1}) << 1);"
+
+def get_rl_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), ((readMem(readReg(HL)) << 1 ) & (u8)flag(C)) | (u8)flag(C));"
+    else:
+        return "writeReg({operand1}, (readReg({operand1})  << 1 ) & (u8)flag(C)) | (u8)flag(C));"
+
+def get_rr_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), (readMem(readReg(HL)) >> 1 ) | ((u8)flag(C) << 7));"
+    else:
+        return "writeReg({operand1}, (readReg({operand1})  >> 1 ) | ((u8)flag(C) << 7));"
+
+def get_sla_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "setFlag(C, readMem(readReg(HL)) >> 7);\n" + "writeMem(readReg(HL), readMem(readReg(HL)) << 1);"
+    else:
+        return "setFlag(C, readReg({operand1}) >> 7);\n" + "writeReg({operand1}, readReg({operand1}) << 1));"
+
+def get_sra_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "setFlag(C, readMem(readReg(HL)) & 1);\n" + "writeMem(readReg(HL), (readMem(readReg(HL)) >> 1) | (readMem((readReg(HL)) >> 7) << 7) );"
+    else:
+        return "setFlag(C, readReg({operand1}) & 1);\n" + "writeReg({operand1}, (readReg({operand1}) >> 1) | (readReg({operand1}) >> 7) << 7) );"
+
+def get_swap_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), swapLowHighBits(readMem(readReg(HL))));"
+    else:
+        return "writeReg({operand1}, swapLowHighBits(readReg({operand1})));"
+
+def get_srl_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "setFlag(C, readMem(readReg(HL)) & 1);\n" + "writeMem(readReg(HL), readMem(readReg(HL)) >> 1);"
+    else:
+        return "setFlag(C, readReg({operand1}) & 1);\n" + "writeReg({operand1}, readReg({operand1}) >> 1);"
+
+def get_res_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), readMem(readReg(HL)) & ~(1 << byte2));"
+    else:
+        return "writeReg({operand1}, readReg({operand1}) & ~(1 << byte2));"
+
+def get_set_body(info):
+    operand1 = info["operands"][0]
+
+    if operand1 == "[HL]":
+        return "writeMem(readReg(HL), readMem(readReg(HL)) | (1 << byte2));"
+    else:
+        return "writeReg({operand1}, readReg({operand1}) | (1 << byte2));"
--- pythonObrada/obrada.py
+++ pythonObrada/obrada.py
@@ -11,13 +11,15 @@ def process_category(opcodes, category):
         instrc = f"0x{i:02X}"
         curr_json = opcodes.get(category, {}).get(instrc, {})
         
-        if not curr_json:
+        if not curr_json or curr_json.get("mnemonic", "").startswith("ILLEGAL"):
             continue
         
         instructions[i] = {
             "mnemonic": curr_json.get("mnemonic"),
             "bytes": curr_json.get("bytes"),
             "operands": [
+            f"[{operand['name']}+]" if not operand.get("immediate", True) and operand.get("increment", False) else
+            f"[{operand['name']}-]" if not operand.get("immediate", True) and operand.get("decrement", False) else
             f"[{operand['name']}]" if not operand.get("immediate", True) else
             f"{operand['name']}+" if operand.get("increment", False) else
             f"{operand['name']}-" if operand.get("decrement", False) else
@@ -46,6 +48,24 @@ def get_mnemonic_operands(instructions):
 
     return operands_by_mnemonic
 
+def get_mnemonic_flags(instructions):
+    flags_by_mnemonic = {}
+
+    for category, instrs in instructions.items():
+        for instr in instrs.values():
+            mnemonic = instr["mnemonic"]
+            flags = instr["flags"]
+
+            if mnemonic not in flags_by_mnemonic:
+                flags_by_mnemonic[mnemonic] = {"Z": set(), "N": set(), "H": set(), "C": set()}
+
+            if flags:
+                for flag, change in flags.items():
+                    if flag in flags_by_mnemonic[mnemonic]:
+                        flags_by_mnemonic[mnemonic][flag].add(change)
+
+    return flags_by_mnemonic
+
 def main():
     input_file = "gameboyOPCodes.json"
     opcodes = load_opcodes(input_file)
@@ -56,11 +76,12 @@ def main():
     }
 
     operands_by_mnemonic = get_mnemonic_operands(instructions)
+    flags_by_mnemonic = get_mnemonic_flags(instructions)
 
-    for mnemonic, operands in operands_by_mnemonic.items():
-        first_operands = ', '.join(operands["first"])
-        not_first_operands = ', '.join(operands["not_first"])
-        print(f"Mnemonic: {mnemonic},\n\tFirst Operands: {first_operands},\n\tNot First Operands: {not_first_operands}")
+    for mnemonic, flags in flags_by_mnemonic.items():
+        flags_str = ', '.join(f"{flag}: {', '.join(changes)}"
+         for flag, changes in flags.items())
+        print(f"Mnemonic: {mnemonic},\n\tFlags: {flags_str}")
 
     # print(instructions)
 
